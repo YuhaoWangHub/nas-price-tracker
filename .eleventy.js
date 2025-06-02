a9eb84294ce0e0ec3173d522e4f19a578963533f
YuhaoWangHub <77960243+YuhaoWangHub@users.noreply.github.com>
2025-06-02 02:10:18 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const fs = require("fs");
+
 module.exports = function (eleventyConfig) {
+  // Copy the data file to the _site/data folder
+  eleventyConfig.addPassthroughCopy({ "data/products.json": "data/products.json" });
+
+  // Load the JSON for use in templates
   eleventyConfig.addGlobalData("products", () => require("./data/products.json"));
+
   return {
     dir: {
       input: "src",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
