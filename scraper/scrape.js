f5582899ef01548d809e0a4dc20c1ff0d952da8b
YuhaoWangHub <77960243+YuhaoWangHub@users.noreply.github.com>
2025-06-02 00:07:12 -0400

    Update scrape.js

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -2,9 +2,15 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+
   const page = await browser.newPage();
-  await page.goto("https://www.amazon.ca/s?k=NAS", { waitUntil: "domcontentloaded" });
+  await page.goto("https://www.amazon.ca/s?k=NAS", {
+    waitUntil: "domcontentloaded"
+  });
 
   const results = await page.evaluate(() => {
     const items = Array.from(document.querySelectorAll(".s-result-item"));
